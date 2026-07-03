3c84673f258f7e4f32a4ef92cae40279abd941c3
Abhi-n4v <abhinav.b.pillai1@gmail.com>
2026-07-03 10:20:48 +0530

    Update INTERN.py

--- INTERN.py
+++ INTERN.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 # take a user input
+st.title("take the input")
 name=st.text_input("Enter your name")
 
-st.title("take the input")
 if st.button("subjects:"):
   st.write(f"print the name: (name)")
